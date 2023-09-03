9767ab90ec760ab5fd140a76aa87f63e43f608ce
shadowandneko <li.zhang@duocuc.cl>
2023-09-03 15:02:04 -0400

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,27 @@
-// src/app/pages/home/home.page.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage {
   constructor(private router: Router) {}
 
-  ionViewDidEnter() {
+  ngOnInit() {
+    const typingEffect = document.querySelector('.typing-effect');
+    const spans = typingEffect?.querySelectorAll('span');
+
+    if (spans) {
+      spans.forEach((span, index) => {
+        span.style.setProperty('--char-index', index.toString());
+      });
+    }
+
     setTimeout(() => {
       this.router.navigateByUrl('/login');
-    }, 5000);
+    }, 3000);
   }
 }
